3ae1878c70c0de86a442a8173c3cc5c2891ac956
ragsraghu <your.ragsraghu1216@gmail.com>
2018-02-13 17:19:40 +0530

    CSS Added

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,7 @@ class Product < ApplicationRecord
 	belongs_to :category
 
 	has_many :product_images, dependent: :destroy
-	accepts_nested_attributes_for :product_images, allow_destroy: true	
+	accepts_nested_attributes_for :product_images, allow_destroy: true
 	
 	validates_presence_of :title
    	validates_numericality_of :price, :message=>"Error Message"
